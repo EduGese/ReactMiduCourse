87151cddbfaffbe51eb5b524973df294cf63d1d6
EduGese <122921699+EduGese@users.noreply.github.com>
2024-08-21 17:43:25 +0200

    End project mouse follower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+const [enabled, setEnabled] = useState(false);
+const [position, setPosition] = useState({x:0, y:0});
+
+useEffect(() => {
+  console.log('effect', {enabled})
+
+  const handleMove  = (event) =>{
+    const {clientX, clientY} = event;
+    console.log('handleMove', {clientX, clientY})
+    setPosition({x: clientX, y: clientY})
+    
+  }
+  if(enabled){//Subscripcion al evento si el estado de enabled es true 
+    window.addEventListener('pointermove', handleMove)
+  }
+  // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    
+  return () => {// cleanup method
+    console.log('cleanup')
+    window.removeEventListener('pointermove', handleMove)
+  }
+}, [enabled])
+
+ // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: '#09f',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity:0.8,
+      pointerEvents: 'none',
+      left:-20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}/>
+    <button onClick={()=>setEnabled(!enabled)} >
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+   
+    
   )
 }
 
